aa76b81ac163e3a291fe6165878fc4e72e77a81f
Haziq <hbnordin2@gmail.com>
2016-11-26 16:40:25 +1000

    Initial commit

--- LossFunctions/svm.py
+++ LossFunctions/svm.py
@@ -2,41 +2,47 @@ import numpy as np
 
 
 class MultiClassSVM:
-    def __init__(self, delta, reg_func):
-        """
-
-        :param delta:
-        :type delta:
-        :param reg_func:
-        :type reg_func:
-        """
+    def __init__(self, delta=1, reg_func=None):
         self.delta = delta
-        self.regFunc = reg_func
+        self.reg_func = reg_func
 
-    @staticmethod
-    def get_example_score(score_vector, correct_vector):
+    def get_example_loss(self, score_vector: np.array, answer_index: np.array) -> int:
         """
+        Given the score vector resulting from a data example, and the index of the correct class for that example,
+        returns the score loss resulting from that example
 
         :param score_vector: A vector of scores
         :type score_vector: numpy.array
-        :param correct_vector: A vector that contains a 1 at the index of the correct answer and 0 everywhere else
-        :type correct_vector: numpy.array
+        :param answer_index: A vector that contains a 1 at the index of the correct answer and 0 everywhere else
+        :type answer_index: numpy.array
         """
-        for i in range(len(score_vector)):
-            if correct_vector[i] == 1:
-                continue
+        correct_class_score = score_vector[answer_index]
+        element_loss = np.maximum(0, score_vector - correct_class_score + self.delta)
+        element_loss[answer_index] = 0
+        return element_loss.sum()
 
+class SvmRegularization:
     @staticmethod
-    def __get_index_of_correct_answer__(correct_vector: np.array) -> int:
+    def get_l2():
         """
+        Returns an L2 regularization function
 
-        :param correct_vector: A vector corresponding to the correct answer for an example, with 1 at that
-        index and 0 everywhere else
-        :type correct_vector: numpy.array
+        :return: An L2 regularization function
+        :rtype: A function
         """
-        if correct_vector.sum() != 1:
-            raise Exception("Correct vector can only contain a single 1 to indicate correct class")
-        return 2
+        return L2Regularization.regularize
 
-svm = MultiClassSVM(1, 2)
-svm.__get_index_of_correct_answer__(np.array([1, 0, 0]))
+
+class L2Regularization:
+    @staticmethod
+    def regularize(weights: np.matrix) -> int:
+        """
+        Takes in a matrix of weights and computers the squared sum of all the values in it
+
+        :param weights: A matrix of weights
+        :type weights: numpy.matrix
+        :return: The weighted sum of all the values in the matrix
+        :rtype: int
+        """
+        squared_weights = np.square(weights)
+        return np.sum(squared_weights)
--- LossFunctions/svmTest.py
+++ LossFunctions/svmTest.py
@@ -1,3 +1,34 @@
 import unittest
+import LossFunctions.svm as svm
+import numpy as np
+
 
 class TestSvm(unittest.TestCase):
+    def setUp(self):
+        self.svm = svm.MultiClassSVM(1, "stub")
+
+
+class TestGetScoreFunction(TestSvm):
+    def runTest(self):
+        self.test_correct_example_score()
+        self.test_wrong_example_score()
+
+    def test_correct_example_score(self):
+        score = np.array([2, 0, 0])
+        answer = 0
+        self.assertEqual(self.svm.get_example_loss(score, answer), 0, "Accurate for correct score")
+
+    def test_wrong_example_score(self):
+        score = np.array([1, 2, 0])
+        answer = 0
+        self.assertEqual(self.svm.get_example_loss(score, answer), 2, "Accurate for incorrect score")
+
+
+class TestL2Regularization(TestSvm):
+    def runTest(self):
+        self.test_values()
+
+    def test_values(self):
+        weights = np.matrix([[1, 2], [3, 4]])
+        squared_sum = svm.L2Regularization.regularize(weights)
+        self.assertEqual(squared_sum, 30, "L2 regularization returns incorrect value")
